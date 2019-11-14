f7790393c3666b09e49552b2df38b82a5d740e1f
llchrisll <h0und3y3@gmail.com>
2019-11-14 18:23:48 +0100

    Core and itemInfo Update: - Synced llchrisll's/kRO folders with latest update - Adjust some lines in lua_text_coding.txt - Finished translating EDDA Bio Lab Weapons and Enchantments [Divine-Pride.net](https://www.divine-pride.net/forum/index.php?/topic/3592-kro-edda-biolab-enchant/) - Basic Adjustments of existing Translations so far - A few jRO Translations - A few Costume Translations - Renaming of Manteau of The Fallen (20748+20749) to Fallen Warrior Manteau - A few Translations regarding 200/70 patch content (Odin Temple & Abyss Lake)
    Signed-off-by: llchrisll <h0und3y3@gmail.com>

---
+++ llchrisll's/kRO/data/luafiles514/decoded/lua files/quest/localquest/local_malayaquest_list.lua
@@ -0,0 +1,629 @@
+LOCAL_MalayaQuest_List = {
+					{
+						name = [[포트말라야]],
+						imagefile = [[ep_test_sample.bmp]],
+						list = {
+									{
+										name  =	[[경계하는 마을]],
+										list = {
+												{
+													name =[[경계하는 마을]],
+													scrfilename = [[malayaQuest]],
+													questID = 7350,
+												},
+												{
+													name =[[그들이 단추를 원하는 이유]],
+													scrfilename = [[malayaQuest]],
+													questID = 7358,
+												},
+												{
+													name =[[내 낡은 단추보다 좋은 것]],
+													scrfilename = [[malayaQuest]],
+													questID = 7351,
+												},
+												{
+													name =[[선원에게 성물 전달]],
+													scrfilename = [[malayaQuest]],
+													questID = 7359,
+												},
+												{
+													name =[[노인에게 성물 전달]],
+													scrfilename = [[malayaQuest]],
+													questID = 7360,
+												},
+												{
+													name =[[청년에게 성물 전달]],
+													scrfilename = [[malayaQuest]],
+													questID = 7361,
+												},
+												{
+													name =[[상인에게 성물 전달]],
+													scrfilename = [[malayaQuest]],
+													questID = 7362,
+												},
+												{
+													name =[[꼬마에게 성물 전달]],
+													scrfilename = [[malayaQuest]],
+													questID = 7363,
+												},
+												{
+													name =[[중년에게 성물 전달]],
+													scrfilename = [[malayaQuest]],
+													questID = 7364,
+												},
+												{
+													name =[[내 낡은 단추보다 좋은 것-2]],
+													scrfilename = [[malayaQuest]],
+													questID = 7365,
+												},
+											}
+									},
+									{
+										name  =	[[전통 수호물과 얄미운 소녀]],
+										list = {
+												{
+													name =[[전통 수호물과 얄미운 소녀-1]],
+													scrfilename = [[malayaQuest]],
+													questID = 7366,
+												},
+												{
+													name =[[전통 수호물과 얄미운 소녀-2]],
+													scrfilename = [[malayaQuest]],
+													questID = 7367,
+												},
+												{
+													name =[[사랑과 수호물은 모두에게]],
+													scrfilename = [[malayaQuest]],
+													questID = 7374,
+												},
+												{
+													name =[[수호물을 전달하라-페드로]],
+													scrfilename = [[malayaQuest]],
+													questID = 7368,
+												},
+												{
+													name =[[수호물을 전달하라-너르도]],
+													scrfilename = [[malayaQuest]],
+													questID = 7369,
+												},
+												{
+													name =[[수호물을 전달하라-판도이]],
+													scrfilename = [[malayaQuest]],
+													questID = 7370,
+												},
+												{
+													name =[[수호물을 전달하라-워언]],
+													scrfilename = [[malayaQuest]],
+													questID = 7371,
+												},
+												{
+													name =[[수호물을 전달하라-탈라]],
+													scrfilename = [[malayaQuest]],
+													questID = 7372,
+												},
+												{
+													name =[[수호물을 전달하라-로멜]],
+													scrfilename = [[malayaQuest]],
+													questID = 7373,
+												},
+												{
+													name =[[사랑과 수호물은 내일도]],
+													scrfilename = [[malayaQuest]],
+													questID = 7375,
+												},
+											}
+									},
+									{
+										name  =	[[전통 수호물 제작을 위해]],
+										list = {
+												{
+													name =[[전통 수호물 제작을 위해]],
+													scrfilename = [[malayaQuest]],
+													questID = 7380,
+												},
+												{
+													name =[[수호물의 재료는 언제나 필요해]],
+													scrfilename = [[malayaQuest]],
+													questID = 7381,
+												},
+											}
+									},
+									{
+										name  =	[[무쇠솥-정기교역]],
+										list = {
+												{
+													name =[[노인과 무쇠솥-1]],
+													scrfilename = [[malayaQuest]],
+													questID = 7376,
+												},
+												{
+													name =[[노인과 무쇠솥-2]],
+													scrfilename = [[malayaQuest]],
+													questID = 7377,
+												},
+												{
+													name =[[노인과 무쇠솥-정기교역]],
+													scrfilename = [[malayaQuest]],
+													questID = 7378,
+												},
+											}
+									},
+									{
+										name  =	[[제제링 퇴치]],
+										list = {
+												{
+													name =[[제제링 퇴치 작전]],
+													scrfilename = [[malayaQuest]],
+													questID = 7404,
+												},
+												{
+													name =[[제제링 퇴치 -내일 이시간에]],
+													scrfilename = [[malayaQuest]],
+													questID = 7405,
+												},
+											}
+									},
+									{
+										name  =	[[제제링과 제젤로피]],
+										list = {
+												{
+													name =[[제제링과 제젤로피]],
+													scrfilename = [[malayaQuest]],
+													questID = 7391,
+												},
+												{
+													name =[[제젤로피 정기수집]],
+													scrfilename = [[malayaQuest]],
+													questID = 7392,
+												},
+											}
+									},
+									{
+										name  =	[[반짝반짝 실버 블레이드]],
+										list = {
+												{
+													name =[[반짝 반짝 실버블레이드]],
+													scrfilename = [[malayaQuest]],
+													questID = 7393,
+												},
+												{
+													name =[[내일도 반짝반짝]],
+													scrfilename = [[malayaQuest]],
+													questID = 7394,
+												},
+											}
+									},
+									{
+										name  =	[[마리스의 아이]],
+										list = {
+												{
+													name =[[마리스의 아이]],
+													scrfilename = [[malayaQuest]],
+													questID = 7388,
+												},
+												{
+													name =[[마리스의 아이에게 옷을]],
+													scrfilename = [[malayaQuest]],
+													questID = 7389,
+												},
+												{
+													name =[[마리스의 아이가 걱정된다]],
+													scrfilename = [[malayaQuest]],
+													questID = 7390,
+												},
+											}
+									},
+									{
+										name  =	[[동의 부탁]],
+										list = {
+												{
+													name =[[동의 부탁-뼈다귀를 모아줘!]],
+													scrfilename = [[malayaQuest]],
+													questID = 7406,
+												},
+												{
+													name =[[정제된 뼈다귀]],
+													scrfilename = [[malayaQuest]],
+													questID = 7407,
+												},
+												{
+													name =[[동의 부탁 - 내일 또 와줄래?]],
+													scrfilename = [[malayaQuest]],
+													questID = 7408,
+												},
+											}
+									},
+									
+									{
+										name  =	[[페리호의 영혼]],
+										list = {
+												{
+													name =[[페리호의 귀신소동]],
+													scrfilename = [[malayaQuest]],
+													questID = 7395,
+												},
+												{
+													name =[[페리호의 화내는 영혼]],
+													scrfilename = [[malayaQuest]],
+													questID = 7396,
+												},
+												{
+													name =[[뭄바키 퐁의 조언]],
+													scrfilename = [[malayaQuest]],
+													questID = 7397,
+												},
+												{
+													name =[[뭄바키 추천 헌화]],
+													scrfilename = [[malayaQuest]],
+													questID = 7398,
+												},
+												{
+													name =[[소녀 디와타의 이야기]],
+													scrfilename = [[malayaQuest]],
+													questID = 7399,
+												},
+												{
+													name =[[페리운항 아직은 괜찮아]],
+													scrfilename = [[malayaQuest]],
+													questID = 7401,
+												},
+											}
+									},
+									{
+										name  =	[[페리 안정화]],
+										list = {
+												{
+													name =[[디와타를 위한 꽃다발]],
+													scrfilename = [[malayaQuest]],
+													questID = 7402,
+												},
+												{
+													name =[[안정화 된 페리]],
+													scrfilename = [[malayaQuest]],
+													questID = 7403,
+												},
+												{
+													name =[[페리운항 아직은 괜찮아]],
+													scrfilename = [[malayaQuest]],
+													questID = 7401,
+												},
+											}
+									},
+									{
+										name  =	[[그 녀석과 눈 마주칠 수 없다.]],
+										list = {
+												{
+													name =[[그 녀석과 눈 마주칠 수 없다!]],
+													scrfilename = [[malayaQuest]],
+													questID = 7409,
+												},
+												{
+													name =[[내일 또 혼내다오!]],
+													scrfilename = [[malayaQuest]],
+													questID = 7410,
+												},
+											}
+									},
+									{
+										name  =	[[바코나와 호수 입장]],
+										list = {
+												{
+													name =[[소문과 세월과 전설]],
+													scrfilename = [[malayaQuest]],
+													questID = 1174,
+												},
+												{
+													name =[[소문과 세월과 전설]],
+													scrfilename = [[malayaQuest]],
+													questID = 1175,
+												},
+												{
+													name =[[소문과 세월과 전설]],
+													scrfilename = [[malayaQuest]],
+													questID = 1176,
+												},
+												{
+													name =[[소문과 세월과 전설]],
+													scrfilename = [[malayaQuest]],
+													questID = 1177,
+												},
+												{
+													name =[[소문과 세월과 전설]],
+													scrfilename = [[malayaQuest]],
+													questID = 1178,
+												},
+												{
+													name =[[소문과 세월과 전설]],
+													scrfilename = [[malayaQuest]],
+													questID = 1179,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1180,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1181,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1182,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1183,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1184,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1185,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1186,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1187,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1188,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1189,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1190,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1191,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1192,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1193,
+												},
+												{
+													name =[[버코너워를 물리쳐라!]],
+													scrfilename = [[malayaQuest]],
+													questID = 1193,
+												},
+											}
+									},
+									{
+										name  =	[[포트말라야의 간호사]],
+										list = {
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11284,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11285,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11286,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11287,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11288,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11289,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11290,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11291,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11292,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11293,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11294,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11295,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11296,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11297,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11298,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11299,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11300,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11301,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11302,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11303,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11304,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11305,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11306,
+												},
+												{
+													name =[[아주 무거운 짐]],
+													scrfilename = [[malayaQuest]],
+													questID = 11207,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11308,
+												},
+												{
+													name =[[포트말라야의 간호사]],
+													scrfilename = [[malayaQuest]],
+													questID = 11309,
+												},
+											}
+									},
+									{
+										name  =	[[벙웅곳을 퇴치하라.]],
+										list = {
+												{
+													name =[[병원 2층, 벙웅곳을 퇴치하라]],
+													scrfilename = [[malayaQuest]],
+													questID = 9222,
+												},
+												{
+													name =[[병원에는 평화가 찾아왔을까..?]],
+													scrfilename = [[malayaQuest]],
+													questID = 9223,
+												},
+												{
+													name =[[병원 2층 탐사]],
+													scrfilename = [[malayaQuest]],
+													questID = 9224,
+												},
+											}
+									},
+									{
+										name  =	[[숲속의 비밀]],
+										list = {
+												{
+													name =[[숲 속의 비밀]],
+													scrfilename = [[malayaQuest]],
+													questID = 2271,
+												},
+												{
+													name =[[숲 속의 비밀]],
+													scrfilename = [[malayaQuest]],
+													questID = 2272,
+												},
+												{
+													name =[[숲 속의 비밀]],
+													scrfilename = [[malayaQuest]],
+													questID = 2273,
+												},
+												{
+													name =[[숲 속의 비밀]],
+													scrfilename = [[malayaQuest]],
+													questID = 2274,
+												},
+												{
+													name =[[숲 속의 비밀]],
+													scrfilename = [[malayaQuest]],
+													questID = 2275,
+												},
+												{
+													name =[[숲 속의 비밀]],
+													scrfilename = [[malayaQuest]],
+													questID = 2276,
+												},
+												{
+													name =[[숲 속의 비밀]],
+													scrfilename = [[malayaQuest]],
+													questID = 2277,
+												},
+												{
+													name =[[숲 속의 비밀]],
+													scrfilename = [[malayaQuest]],
+													questID = 2278,
+												},
+												{
+													name =[[숲 속의 비밀]],
+													scrfilename = [[malayaQuest]],
+													questID = 2279,
+												},
+												{
+													name =[[숲 속의 비밀]],
+													scrfilename = [[malayaQuest]],
+													questID = 2280,
+												},
+											}
+									},
+							},
+					},
+		}
