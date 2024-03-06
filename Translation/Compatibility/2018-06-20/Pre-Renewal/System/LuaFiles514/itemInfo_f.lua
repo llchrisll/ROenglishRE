c537f6373b57a401564aa5440973bbcbaf200dc7
Houndeye <h0und3y3@gmail.com>
2024-03-06 09:31:46 +0100

    Minor Update - 06 March 2024 - Fix a mistake from my side

--- Translation/Compatibility/2018-06-20/Pre-Renewal/System/LuaFiles514/itemInfo_f.lua
+++ Translation/Compatibility/2018-06-20/Pre-Renewal/System/LuaFiles514/itemInfo_f.lua
@@ -1,6 +1,10 @@
 function main()
 	for ItemID, DESC in pairs(tbl) do
-		result, msg = AddItem(ItemID, DESC.unidentifiedDisplayName, DESC.unidentifiedResourceName, DESC.identifiedDisplayName, DESC.identifiedResourceName, DESC.slotCount, DESC.ClassNum)
+		if DispayOrigin == 1 and DESC.Server ~= nil then
+			result, msg = AddItem(ItemID, DESC.unidentifiedDisplayName, DESC.unidentifiedResourceName, DESC.identifiedDisplayName..' '..DESC.Server, DESC.identifiedResourceName, DESC.slotCount, DESC.ClassNum)
+		else
+			result, msg = AddItem(ItemID, DESC.unidentifiedDisplayName, DESC.unidentifiedResourceName, DESC.identifiedDisplayName, DESC.identifiedResourceName, DESC.slotCount, DESC.ClassNum)
+		end
 		if not result == true then
 			return false, msg
 		end
@@ -10,10 +14,31 @@ function main()
 				return false, msg
 			end
 		end
+		if DispayOrigin == 2 or DisplayItemID == 1 then
+			if DispayOrigin == 2 and DESC.Server ~= nil then
+				AddItemIdentifiedDesc(ItemID, "^0000CCServer: ^FF0000"..DESC.Server.."^000000")
+			end
+			if DisplayItemID == 1 then
+				AddItemIdentifiedDesc(ItemID, "^0000CCItem ID:^000000 "..ItemID)
+			end
+			AddItemIdentifiedDesc(ItemID, "________________________")
+		end
 		for k, v in pairs(DESC.identifiedDescriptionName) do
 			result, msg = AddItemIdentifiedDesc(ItemID, v)
 			if not result == true then
 				return false, msg
+			end
+		end
+		if DispayOrigin == 3 or DisplayItemID == 2 or DisplayDatabase == true then
+			AddItemIdentifiedDesc(ItemID, "________________________")
+			if DispayOrigin == 3 and DESC.Server ~= nil then
+				AddItemIdentifiedDesc(ItemID, "^0000CCServer: ^FF0000"..DESC.Server.."^000000")
+			end
+			if DisplayItemID == 2 then
+				AddItemIdentifiedDesc(ItemID, "^0000CCItem ID:^000000 "..ItemID)
+			end
+			if DisplayDatabase == true then
+				AddItemIdentifiedDesc(ItemID, "<URL>Divine-Pride.net<INFO>https://www.divine-pride.net/database/item/"..ItemID.."</INFO></URL>")
 			end
 		end
 		if nil ~= DESC.EffectID then
