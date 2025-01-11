23073800ac14156cfc6393f5e7892045302e79d6
Houndeye <h0und3y3@gmail.com>
2025-01-11 21:36:09 +0100

    Translation Update - 11th January 2025 - Added an new main function required for the whole project as well as the Custom Lua Support, SystemEN/LuaFiles514/rotp_f.lua   Updated the itemInfo.lua to use the new function file instead and made it easier to add new item files, see SystemEN/itemInfo.lua - Lots of updates regarding Custom Lua Support (CLS)   Removed navigation support   Updated the way of reading the cls files, currently only the working files were updated.   (Still not done testing various files, Lapine Boxes, Hateffects (these are annoying), World Map atm)   Rest of the files just got a synced header - Removed an misplaced skilltreeview.lub in Additions folder - Added and translated Furious Weapons and ETC items as well as 2 costume garments with hateffects to test... - Updated the skillid.lub in Additions folder

---
+++ Translation/Renewal/SystemEN/LuaFiles514/rotp_f.lua
@@ -0,0 +1,67 @@
+-- Original translation works of zackdreaver: https://github.com/zackdreaver/ROenglishRE
+-- Continuated by llchrisll at https://github.com/llchrisll/ROenglishRE
+-- This file can be distributed, used and modified freely
+-- This file shouldn't be claimed as part of your project, unless you fork it from https://github.com/llchrisll/ROenglishRE
+
+-----------     DON'T TOUCH THIS FILE AT ALL     -----------
+----------- IF YOU DON'T KNOW WHAT YOU ARE DOING -----------
+--[[ F_LuaTableMerge
+	Merges a custom table into the main table
+	@param source table
+	@param dest table
+]]
+function F_LuaTableMerge(source,dest,state)
+	if source == nil or dest == nil then
+		return 0
+	end
+	for k,v in pairs(source) do
+		dest[k] = v
+	end
+return #dest
+end
+
+--[[ F_LuaTableInsert
+	Replaces the last entry of the original table with the first custom entry
+	then adds the others, mostly used for `transparentItem` and other similar tables.
+	@param source table
+	@param dest table
+]]
+function F_LuaTableInsert(source,dest)
+	if source == nil or dest == nil then
+		if dest == nil then
+			error(dest..' table not loaded')
+		end
+		return 0
+	end
+	local ct = #dest
+	for _, v in pairs(source) do
+		dest[ct] = v
+		ct = ct + 1
+	end
+	return #dest
+end
+
+--[[ F_itemInfoMerge(src, state)
+	Merges a custom tables into the main table
+	@param src table to merge into tbl
+	@param state bolean overwrite existing entries (true) or not (false)
+]]
+function F_itemInfoMerge(src, state)
+	if src == nil then
+		return
+	end
+	for ItemID,DESC in pairs(src) do
+		if state == false then
+			if not tbl[ItemID] then
+				tbl[ItemID] = {}
+				tbl[ItemID] = DESC
+			end
+		else
+			tbl[ItemID] = DESC
+		end
+		if src == tbl_custom then
+			tbl[ItemID].Custom = true
+		end
+	end
+	return
+end
