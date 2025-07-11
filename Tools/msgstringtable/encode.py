71528a28d8946e4ab8473d694df50d2b1dbbae19
llchrisll <h0und3y3@gmail.com>
2025-07-11 18:56:05 +0200

    Minor Update - 11th July 2025 - Changed extension of runeset_reward.lua to runeset_reward.lub - Renamed msgstringtable_decoded.txt to msgstringtable.txt - Adjusted the encode.py respectively - Fixed some syntax issues within the Rune folder - Updated some strings in msgstringtable.txt and .csv - Removed an accidental line in Pre-Re skilldescript.lub regarding `KN_TWOHANDQUICKEN` - Updated latest changed in skillinfoz from kRO, changing `MH_SONIC_CRAW` to `MH_SONIC_CLAW` and `BA_FROSTJOKE` to `BA_FROSTJOKER`   Fixing both lua errors with line 7643(skillinfolist) and 4959(skilldescript)

--- Tools/msgstringtable/encode.py
+++ Tools/msgstringtable/encode.py
@@ -1,7 +1,7 @@
 import csv
 import base64
 
-with open('msgstringtable.csv', 'w', newline='') as outfile, open('msgstringtable_decoded.txt', newline='') as decoded:
+with open('msgstringtable.csv', 'w', newline='') as outfile, open('msgstringtable.txt', newline='') as decoded:
     rdr = csv.reader(decoded, delimiter=',')
     for row in rdr:
         idx = base64.b64encode(row[0].encode('utf-8')).decode('utf-8')
