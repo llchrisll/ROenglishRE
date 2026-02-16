ee3a1e804ea473ff1719d6878777a96d41db0fe4
llchrisll <h0und3y3@gmail.com>
2026-02-16 21:39:18 +0100

    Updates to rotp_f.lua and Skill Rebalance for Shinkiro/Shiraniu - Combined F_LuaTableMerge and F_LuaTableInsert into F_ROTP - Made adjustments in all files which made use of these two functions - Removed cls/enumvar.lub and combined it with cls/randomoption.lub - Updated some strings in msgstringtable files - Updated skills based on Skill Rebalances of Shinkiro/Shiranui   [1](https://www.divine-pride.net/forum/index.php?/topic/3723-kro-jobs-improvement-project/page/14/)   [2](https://www.divine-pride.net/forum/index.php?/topic/3723-kro-jobs-improvement-project/page/15/)

--- Translation/Renewal/SystemEN/LuaFiles514/rotp_f.lua
+++ Translation/Renewal/SystemEN/LuaFiles514/rotp_f.lua
@@ -5,47 +5,65 @@
 
 -----------     DON'T TOUCH THIS FILE AT ALL     -----------
 ----------- IF YOU DON'T KNOW WHAT YOU ARE DOING -----------
---[[ F_LuaTableMerge
-	Merges a custom table into the main table
-	@param source table
-	@param dest table
-]]
-function F_LuaTableMerge(source,dest)
-	if source == nil or dest == nil then
-		return 0
-	end
-	for k,v in pairs(source) do
-		dest[k] = v
-	end
-return #dest
-end
 
---[[ F_LuaTableInsert
-	Replaces the last entry of the original table with the first custom entry
-	then adds the others, mostly used for `transparentItem` and other similar tables.
+--[[ F_ROTP
+	Handles merging and overwriting of lua tables
 	@param source table
 	@param dest table
+	@param st integer Optional: 1=Special lua tables, see below
+	@param c_tbl boolean Optional: Should the function respect additional tables (true/false) (default: false)
+	
+	Special Lua Tables:
+	Lua tables like transparentItem have their last entry containing 0 or similiar,
+	so we need to replace that with the first custom entry and re-add that via our table.
 ]]
-function F_LuaTableInsert(source,dest)
+function F_ROTP(source,dest,st,c_tbl)
+	-- Return if source or dest is not provided or source is 0
 	if source == nil or dest == nil then
-		return 0
+		return
 	end
-	local ct = #dest
-	for _, v in pairs(source) do
-		dest[ct] = v
-		ct = ct + 1
+	if st == 1 then -- If it's a special table, get the index of the last entry and merge from there
+		ct = #dest
 	end
-	return #dest
+	if c_tbl == nil then -- Default c_table to false if it not provided
+		c_tbl = false
+	end
+	for k, v in pairs(source) do
+		if type(v) == "table" and c_tbl == true then
+			if not dest[k] then
+				dest[k] = {}
+			end
+			for k2,v2 in pairs(v) do
+				if st == 1 then
+					dest[k][ct] = v2
+					ct = ct + 1
+				else
+					dest[k][k2] = v2
+				end
+			end
+		else
+			if st == 1 then
+				dest[ct] = v
+				ct = ct + 1
+			else
+				dest[k] = v
+			end
+		end
+	end
+	return
 end
 
 --[[ F_itemInfoMerge(src, state)
 	Merges a custom tables into the main table
 	@param src table to merge into tbl
-	@param state bolean overwrite existing entries (true) or not (false)
+	@param state bolean optional: overwrite existing entries (true) or not (false)
 ]]
 function F_itemInfoMerge(src, state)
 	if src == nil then
 		return
+	end
+	if state == nil then -- Default state to false if not provided
+		state = false
 	end
 	for ItemID,DESC in pairs(src) do
 		if state == false then
